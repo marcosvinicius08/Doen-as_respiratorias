640983e7c9532d41a074729a706eedeaf1eca20f
Marcos Vinicius <161716269+marcosvinicius08@users.noreply.github.com>
2026-06-22 13:10:51 -0300

    op

--- Trabalho Programação Lógica/doencas_respiratoria.pl
+++ Trabalho Programação Lógica/doencas_respiratoria.pl
@@ -101,6 +101,31 @@ subconjunto_sintomas(perda_de_sabor, perda_paladar).
 
 subconjunto_sintomas(cansaco, fadiga).
 
+lista_doencas([
+    covid19,
+    gripe,
+    influenza,
+    resfriado,
+    tuberculose,
+    pneumonia,
+    asma,
+    doencas_pulmonares_cronicas,
+    rinite,
+    outras_respiratorias
+]).
+lista_de_sintomas([
+    febre,
+    tosse,
+    falta_de_ar,
+    perda_olfato,
+    fadiga,
+    mal_estar,
+    dor_cabeca,
+    chiado,
+    congestao_nasal,
+    coriza
+]).
+
 naomembro(_, []).
 naomembro(X, [H|T]) :-
     X \= H,
@@ -111,7 +136,150 @@ listar_sintomas(Doenca, Lista) :-
 
 listar_sintomas(Doenca, Acc, Lista) :-
     sintoma(Doenca, S, _, _, _, _, _),
-     naomembro(S, Acc),!,
+     naomembro(S, Acc),
     listar_sintomas(Doenca, [S|Acc], Lista).
 
 listar_sintomas(_, Lista, Lista).
+quais_doencas_possui(Sintoma, Lista) :- quais_doencas_possui(Sintoma,[],Lista).
+quais_doencas_possui(Sintoma,Acc,Lista) :-
+    sintoma(D,Sintoma,_,_,_,_,_), naomembro(D,Acc),
+    quais_doencas_possui(Sintoma,[D|Acc],Lista).
+
+quais_doencas_possui(_,Lista,Lista).
+
+
+peso_classificacao(critico, 2.0).
+peso_classificacao(comum, 1.0).
+peso_classificacao(raro, 0.5).
+
+peso_intensidade(leve, 0.8).
+peso_intensidade(moderada, 1.0).
+peso_intensidade(alta, 1.1).
+peso_intensidade(severa, 1.2).
+
+peso_frequencia(continuo, 1.2).
+peso_frequencia(intermitente, 1.0).
+peso_frequencia(raro, 0.7).
+
+inserir_ordenado((D,S), [], [(D,S)]).
+
+inserir_ordenado((D,S), [(D1,S1)|T], [(D,S),(D1,S1)|T]) :-
+    S >= S1.
+
+inserir_ordenado((D,S), [(D1,S1)|T], [(D1,S1)|R]) :-
+    S < S1,
+    inserir_ordenado((D,S), T, R).
+
+
+ordenar([], []).
+
+ordenar([H|T], R) :-
+    ordenar(T, R1),
+    inserir_ordenado(H, R1, R).
+
+
+ranking_doencas_aux([], _, []).
+ranking_doencas_aux([D|T], S, [(D,Score)|R]) :-
+    score_doenca(D,S,Score),
+    Score > 0,
+    ranking_doencas_aux(T,S,R).
+
+ranking_doencas_aux([_|T], S, R) :-
+    ranking_doencas_aux(T,S,R).
+
+ranking_doencas(SintomasPaciente, Ranking) :-
+    lista_doencas(Lista),
+    ranking_doencas_aux(Lista, SintomasPaciente, Ranking).
+
+calcular_score(P, Class, Int, Freq, Score) :-
+    peso_classificacao(Class, PClass),
+    peso_intensidade(Int, PInt),
+    peso_frequencia(Freq, PFreq),
+    Score is P * PClass * PInt * PFreq.
+
+
+score_sintoma_ou_zero(D, S, Score) :-
+    sintoma(D, S, intensidade(Int), prob(P), _, frequencia(Freq), Class),
+    calcular_score(P, Class, Int, Freq, Score), !.
+
+score_sintoma_ou_zero(D, S, Score) :-
+    subconjunto_sintomas(S, S2),
+    sintoma(D, S2, intensidade(Int), prob(P), _, frequencia(Freq), Class),
+    calcular_score(P, Class, Int, Freq, Score), !.
+
+score_sintoma_ou_zero(_, _, 0).
+
+
+score_doenca(_, [], 0).
+
+score_doenca(D, [H|T], Resultado) :-
+    score_doenca(D, T, R),
+    score_sintoma_ou_zero(D, H, Score),
+    Resultado is R + Score.
+
+
+
+
+diagnosticar_doenca(SintomasPaciente, Resultado) :-
+    ranking_doencas(SintomasPaciente, Ranking),
+    ordenar(Ranking, Resultado).
+
+
+explicar(_, [], []).
+
+explicar(D, [H|T], [(H,P,Class,Int,Freq,Score)|R]) :-
+    sintoma(D,
+            H,
+            intensidade(Int),
+            prob(P),
+            _,
+            frequencia(Freq),
+            Class),
+
+    calcular_score(P, Class, Int, Freq, Score),
+
+    explicar(D, T, R),!.
+explicar(D, [H|T], [(H,P,Class,Int,Freq,Score)|R]) :-
+    subconjunto_sintomas(H, S2),
+    sintoma(D,
+            S2,
+            intensidade(Int),
+            prob(P),
+            _,
+            frequencia(Freq),
+            Class),
+
+    calcular_score(P, Class, Int, Freq, Score),
+
+    explicar(D, T, R),!.
+explicar(D,[_|T],R) :-
+    explicar(D,T,R).
+
+perguntar(Sintoma, Resposta) :-
+    write('Possui '),
+    write(Sintoma),
+    write('? (s/n) '),
+    read(Resposta).
+coletar_sintomas([], []).
+
+coletar_sintomas([H|T], [H|R]) :-
+    perguntar(H, s),
+    !,
+    coletar_sintomas(T, R).
+
+coletar_sintomas([_|T], R) :-
+    coletar_sintomas(T, R).
+consultar :-
+    lista_de_sintomas(Lista),
+    coletar_sintomas(Lista, SintomasPaciente),
+
+    nl,
+    write('Sintomas informados: '),
+    write(SintomasPaciente),
+    nl,
+
+    diagnosticar_doenca(SintomasPaciente, Resultado),
+
+    nl,
+    write('Ranking das doencas:'), nl,
+    write(Resultado), nl.
